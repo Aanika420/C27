5119700b15df101593f076a5dfcb33ec83b0ef87
Aanika420 <aanikav100@gmail.com>
2021-03-11 19:02:36 -0500

    constraint bodies

--- sketch.js
+++ sketch.js
@@ -33,9 +33,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(200,100,100, PI/7);
 
     bird = new Bird(100,100);
 
+    chain = new Chain(log6.body,bird.body);
 }
 
 function draw(){
@@ -59,6 +61,10 @@ function draw(){
     log4.display();
     log5.display();
 
+    log6.display();
+
     bird.display();
     platform.display();
+
+    chain.display();
 }
